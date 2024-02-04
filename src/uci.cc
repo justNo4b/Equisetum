18fbfb871e955adaac93bdbc45e2cf437d2a1f07
justno4b <novosti-wxo@yandex.ru>
2024-02-04 22:27:56 +0300

    info string

--- src/uci.cc
+++ src/uci.cc
@@ -420,6 +420,16 @@ void loop() {
 
 void Uci::init() {
   initOptions();
+
+/* Shit for SPSA
+        for(auto it = optionsMap.cbegin(); it != optionsMap.cend(); ++it)
+        {
+            auto step = (int)((it->second.getMin() + it->second.getMax()) / 20);
+            step = std::max(1, step);
+            std::cout << it->first << " int " << it->second.getDefaultValue() << " " << it->second.getMin() << " " << it->second.getMax() << " " <<  step << " 0.002" << std::endl;
+        }
+*/
+
 }
 
 void Uci::start() {
