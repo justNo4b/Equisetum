b7f9663dbecadf7a3dd32cb1da8646b23beaa8d2
justno4b <novosti-wxo@yandex.ru>
2024-02-08 18:15:44 +0300

    prepare staged

--- src/movegen.cc
+++ src/movegen.cc
@@ -20,16 +20,17 @@
 #include "eval.h"
 
 
-MoveGen::MoveGen(const Board &board, bool isCaptureGenerated) {
-  setBoard(board, isCaptureGenerated);
+MoveGen::MoveGen(const Board *board, bool isCaptureGenerated, MoveList * ml) {
+  _moves = ml;
 }
 
-MoveGen::MoveGen() {
-  setBoard(Board(), false);
+MoveGen::MoveGen(MoveList * ml) {
+    _moves = ml;
+    Board b = Board();
+  setBoard(&b, false);
 }
 
-void MoveGen::setBoard(const Board &board, bool isCaptureGenerated) {
-  _moves = MoveList();
+void MoveGen::setBoard(const Board *board, bool isCaptureGenerated) {
   if (!isCaptureGenerated){
      _genMoves(board);
   } else{
@@ -37,38 +38,30 @@ void MoveGen::setBoard(const Board &board, bool isCaptureGenerated) {
   }
 }
 
-
-MoveList * MoveGen::getMoves() {
-  return  &_moves;
-}
-
-void MoveGen::_genMoves(const Board &board) {
-    _moves.reserve(MOVELIST_RESERVE_SIZE);
-    Color color = board.getActivePlayer();
+void MoveGen::_genMoves(const Board *board) {
+    Color color = board->getActivePlayer();
     Color otherColor = getOppositeColor(color);
 
     _genPawnMoves(board, color);
     _genPawnAttacks(board, color);
-    _genRookMoves(board, board.getPieces(color, ROOK), board.getAttackable(otherColor));
-    _genKnightMoves(board, board.getPieces(color, KNIGHT), board.getAttackable(otherColor));
-    _genBishopMoves(board, board.getPieces(color, BISHOP), board.getAttackable(otherColor));
-    _genKingMoves(board, color, board.getPieces(color, KING), board.getAttackable(otherColor));
-    _genQueenMoves(board, board.getPieces(color, QUEEN), board.getAttackable(otherColor));
+        _genRookMoves(board, board->getPieces(color, ROOK), board->getAttackable(otherColor));
+    _genKnightMoves(board, board->getPieces(color, KNIGHT), board->getAttackable(otherColor));
+    _genBishopMoves(board, board->getPieces(color, BISHOP), board->getAttackable(otherColor));
+    _genKingMoves(board, color, board->getPieces(color, KING), board->getAttackable(otherColor));
+    _genQueenMoves(board, board->getPieces(color, QUEEN), board->getAttackable(otherColor));
 }
 
-void MoveGen::_genCaptures(const Board &board) {
-    _moves.reserve(MOVELIST_RESERVE_SIZE);
-    Color color = board.getActivePlayer();
+void MoveGen::_genCaptures(const Board *board) {
+    Color color = board->getActivePlayer();
     Color otherColor = getOppositeColor(color);
 
     _genPawnAttacks(board, color);
     _getPromQonly(board, color);
-    _genRookCaps(board, board.getPieces(color, ROOK), board.getAttackable(otherColor));
-    _genKnightCaps(board, board.getPieces(color, KNIGHT), board.getAttackable(otherColor));
-    _genBishopCaps(board, board.getPieces(color, BISHOP), board.getAttackable(otherColor));
-    _genKingCaps(board, board.getPieces(color, KING), board.getAttackable(otherColor));
-    _genQueenCaps(board, board.getPieces(color, QUEEN), board.getAttackable(otherColor));
-
+    _genRookCaps(board, board->getPieces(color, ROOK), board->getAttackable(otherColor));
+    _genKnightCaps(board, board->getPieces(color, KNIGHT), board->getAttackable(otherColor));
+    _genBishopCaps(board, board->getPieces(color, BISHOP), board->getAttackable(otherColor));
+    _genKingCaps(board, board->getPieces(color, KING), board->getAttackable(otherColor));
+    _genQueenCaps(board, board->getPieces(color, QUEEN), board->getAttackable(otherColor));
 }
 
 inline void MoveGen::_genPawnPromotions(unsigned int from, unsigned int to, unsigned int flags, PieceType capturedPieceType) {
@@ -79,27 +72,27 @@ inline void MoveGen::_genPawnPromotions(unsigned int from, unsigned int to, unsi
 
   Move queenPromotion = promotionBase;
   queenPromotion.setPromotionPieceType(QUEEN);
-  _moves.push_back(queenPromotion);
+  _moves->push_back(queenPromotion);
 
   Move rookPromotion = promotionBase;
   rookPromotion.setPromotionPieceType(ROOK);
-  _moves.push_back(rookPromotion);
+  _moves->push_back(rookPromotion);
 
   Move bishopPromotion = promotionBase;
   bishopPromotion.setPromotionPieceType(BISHOP);
-  _moves.push_back(bishopPromotion);
+  _moves->push_back(bishopPromotion);
 
   Move knightPromotion = promotionBase;
   knightPromotion.setPromotionPieceType(KNIGHT);
-  _moves.push_back(knightPromotion);
+  _moves->push_back(knightPromotion);
 }
 
-void MoveGen::_genPawnMoves(const Board &board, Color color) {
-  U64 movedPawns = color == WHITE ? board.getPieces(color, PAWN) << 8
-                                  : board.getPieces(color, PAWN) >> 8;
-  movedPawns &= board.getNotOccupied();
-  U64 doublePushes = color == WHITE ? (movedPawns << 8) & board.getNotOccupied() & DOUBLE_PUSH_RANK[color]
-                                    : (movedPawns >> 8) & board.getNotOccupied() & DOUBLE_PUSH_RANK[color];
+void MoveGen::_genPawnMoves(const Board *board, Color color) {
+  U64 movedPawns = color == WHITE ? board->getPieces(color, PAWN) << 8
+                                  : board->getPieces(color, PAWN) >> 8;
+  movedPawns &= board->getNotOccupied();
+  U64 doublePushes = color == WHITE ? (movedPawns << 8) & board->getNotOccupied() & DOUBLE_PUSH_RANK[color]
+                                    : (movedPawns >> 8) & board->getNotOccupied() & DOUBLE_PUSH_RANK[color];
 
   U64 promotions = movedPawns & PROMOTION_RANK[color];
   movedPawns &= ~PROMOTION_RANK[color];
@@ -108,7 +101,7 @@ void MoveGen::_genPawnMoves(const Board &board, Color color) {
   // Generate single non promotion moves
   while (movedPawns) {
     int to = _popLsb(movedPawns);
-    _moves.push_back(Move(to + fromAdj, to, PAWN));
+    _moves->push_back(Move(to + fromAdj, to, PAWN));
   }
 
   // Generate promotions
@@ -119,13 +112,13 @@ void MoveGen::_genPawnMoves(const Board &board, Color color) {
 
   while (doublePushes) {
     int to = _popLsb(doublePushes);
-    _moves.push_back(Move(to + (2 * fromAdj), to, PAWN, Move::DOUBLE_PAWN_PUSH));
+    _moves->push_back(Move(to + (2 * fromAdj), to, PAWN, Move::DOUBLE_PAWN_PUSH));
   }
 }
 
-inline void MoveGen::_getPromQonly(const Board &board, Color color){
-  U64 promotions = color == WHITE ? board.getPieces(color, PAWN) << 8 : board.getPieces(color, PAWN) >> 8;
-  promotions &= board.getNotOccupied();
+inline void MoveGen::_getPromQonly(const Board *board, Color color){
+  U64 promotions = color == WHITE ? board->getPieces(color, PAWN) << 8 : board->getPieces(color, PAWN) >> 8;
+  promotions &= board->getNotOccupied();
   promotions &= PROMOTION_RANK[color];
   int fromAdj = color == WHITE ? -8 : 8;
 
@@ -134,52 +127,52 @@ inline void MoveGen::_getPromQonly(const Board &board, Color color){
     int to = _popLsb(promotions);
     Move m = Move(to + fromAdj, to, PAWN, Move::PROMOTION);
     m.setPromotionPieceType(QUEEN);
-    _moves.push_back(m);
+    _moves->push_back(m);
   }
 }
 
-inline void MoveGen::_genPawnAttacks(const Board &board, Color color) {
+inline void MoveGen::_genPawnAttacks(const Board *board, Color color) {
     U64 promRank = color == WHITE ? RANK_8 : RANK_1;
     Color otherColor = getOppositeColor(color);
-    U64 leftRegularAttacks = color == WHITE ? (board.getPieces(color, PAWN) << 7) & board.getAttackable(otherColor) & ~FILE_H
-                                            : (board.getPieces(color, PAWN) >> 9) & board.getAttackable(otherColor) & ~FILE_H;
+    U64 leftRegularAttacks = color == WHITE ? (board->getPieces(color, PAWN) << 7) & board->getAttackable(otherColor) & ~FILE_H
+                                            : (board->getPieces(color, PAWN) >> 9) & board->getAttackable(otherColor) & ~FILE_H;
     U64 leftAttackPromotions = leftRegularAttacks & promRank;
     leftRegularAttacks &= ~promRank;
     int fromAdj = color == WHITE ? -7 : 9;
 
-    U64 leftEnPassant = color == WHITE  ? (board.getPieces(color, PAWN) << 7) & board.getEnPassant() & ~FILE_H
-                                        : (board.getPieces(color, PAWN) >> 9) & board.getEnPassant() & ~FILE_H;
+    U64 leftEnPassant = color == WHITE  ? (board->getPieces(color, PAWN) << 7) & board->getEnPassant() & ~FILE_H
+                                        : (board->getPieces(color, PAWN) >> 9) & board->getEnPassant() & ~FILE_H;
 
   // Add regular attacks (Not promotions or en passants)
   while (leftRegularAttacks) {
     int to = _popLsb(leftRegularAttacks);
 
     Move move = Move(to + fromAdj, to, PAWN, Move::CAPTURE);
-    move.setCapturedPieceType(board.getPieceAtSquare(otherColor, to));
+    move.setCapturedPieceType(board->getPieceAtSquare(otherColor, to));
 
-    _moves.push_back(move);
+    _moves->push_back(move);
   }
 
   // Add promotion attacks
   while (leftAttackPromotions) {
     int to = _popLsb(leftAttackPromotions);
-    _genPawnPromotions(to + fromAdj, to, Move::CAPTURE, board.getPieceAtSquare(otherColor, to));
+    _genPawnPromotions(to + fromAdj, to, Move::CAPTURE, board->getPieceAtSquare(otherColor, to));
   }
 
   // Add en passant attacks
   // There can only be one en passant square at a time, so no need for loop
   if (leftEnPassant) {
     int to = _popLsb(leftEnPassant);
-    _moves.push_back(Move(to + fromAdj, to, PAWN, Move::EN_PASSANT));
+    _moves->push_back(Move(to + fromAdj, to, PAWN, Move::EN_PASSANT));
   }
 
-    U64 rightRegularAttacks = color == WHITE ? (board.getPieces(color, PAWN) << 9) & board.getAttackable(otherColor) & ~FILE_A
-                                            : (board.getPieces(color, PAWN) >> 7) & board.getAttackable(otherColor) & ~FILE_A;
+    U64 rightRegularAttacks = color == WHITE ? (board->getPieces(color, PAWN) << 9) & board->getAttackable(otherColor) & ~FILE_A
+                                            : (board->getPieces(color, PAWN) >> 7) & board->getAttackable(otherColor) & ~FILE_A;
     U64 rightAttackPromotions = rightRegularAttacks & promRank;
     rightRegularAttacks &= ~promRank;
 
-    U64 rightEnPassant = color == WHITE ? (board.getPieces(color, PAWN) << 9) & board.getEnPassant() & ~FILE_A
-                                        : (board.getPieces(color, PAWN) >> 7) & board.getEnPassant() & ~FILE_A;
+    U64 rightEnPassant = color == WHITE ? (board->getPieces(color, PAWN) << 9) & board->getEnPassant() & ~FILE_A
+                                        : (board->getPieces(color, PAWN) >> 7) & board->getEnPassant() & ~FILE_A;
     fromAdj = color == WHITE ? -9 : 7;
 
   // Add regular attacks (Not promotions or en passants)
@@ -187,35 +180,35 @@ inline void MoveGen::_genPawnAttacks(const Board &board, Color color) {
     int to = _popLsb(rightRegularAttacks);
 
     Move move = Move(to + fromAdj, to, PAWN, Move::CAPTURE);
-    move.setCapturedPieceType(board.getPieceAtSquare(otherColor, to));
+    move.setCapturedPieceType(board->getPieceAtSquare(otherColor, to));
 
-    _moves.push_back(move);
+    _moves->push_back(move);
   }
 
   // Add promotion attacks
   while (rightAttackPromotions) {
     int to = _popLsb(rightAttackPromotions);
-    _genPawnPromotions(to + fromAdj, to, Move::CAPTURE, board.getPieceAtSquare(otherColor, to));
+    _genPawnPromotions(to + fromAdj, to, Move::CAPTURE, board->getPieceAtSquare(otherColor, to));
   }
 
   // Add en passant attacks
   // There can only be one en passant square at a time, so no need for loop
   if (rightEnPassant) {
     int to = _popLsb(rightEnPassant);
-    _moves.push_back(Move(to + fromAdj, to, PAWN, Move::EN_PASSANT));
+    _moves->push_back(Move(to + fromAdj, to, PAWN, Move::EN_PASSANT));
   }
 }
 
-void MoveGen::_genKingMoves(const Board &board, Color color, U64 king, U64 attackable) {
+void MoveGen::_genKingMoves(const Board *board, Color color, U64 king, U64 attackable) {
     // Add normal moves
     int kingIndex = _bitscanForward(king);
-    U64 moves = board.getAttacksForSquare(KING, board.getActivePlayer(), kingIndex);
+    U64 moves = board->getAttacksForSquare(KING, board->getActivePlayer(), kingIndex);
     _addMoves(board, kingIndex, KING, moves, attackable);
 
     // Add Castlings
-    U64 castlingRights = board.getCastlingRightsColored(color);
+    U64 castlingRights = board->getCastlingRightsColored(color);
     // return if we under check
-    if (board.colorIsInCheck(color)) castlingRights = 0;
+    if (board->colorIsInCheck(color)) castlingRights = 0;
 
     while(castlingRights){
         int rookSquare  = _popLsb(castlingRights);
@@ -230,117 +223,117 @@ void MoveGen::_genKingMoves(const Board &board, Color color, U64 king, U64 attac
         U64 toBeFree = rookToKing | kingJumpSq | (ONE << toRook);
         toBeFree = toBeFree & ~(ONE << kingIndex);
         toBeFree = toBeFree & ~(ONE << rookSquare);
-        if (toBeFree & board.getOccupied()) continue;
+        if (toBeFree & board->getOccupied()) continue;
         bool pathAttacked = false;
 
         while (kingJumpSq)
         {
             int sq = _popLsb(kingJumpSq);
-            if (board.squareUnderAttack(getOppositeColor(color), sq)){
+            if (board->squareUnderAttack(getOppositeColor(color), sq)){
                 pathAttacked = true;
                 break;
                 }
         }
         Move::Flag flag = rookSquare > kingIndex ? Move::KSIDE_CASTLE : Move::QSIDE_CASTLE;
-        if (!pathAttacked) _moves.push_back(Move(kingIndex, rookSquare, KING, flag));
+        if (!pathAttacked) _moves->push_back(Move(kingIndex, rookSquare, KING, flag));
     }
 }
 
-inline void MoveGen::_genKingCaps(const Board &board, U64 king, U64 attackable) {
+inline void MoveGen::_genKingCaps(const Board *board, U64 king, U64 attackable) {
 
   int kingIndex = _bitscanForward(king);
 
-  U64 moves = board.getAttacksForSquare(KING, board.getActivePlayer(), kingIndex);
+  U64 moves = board->getAttacksForSquare(KING, board->getActivePlayer(), kingIndex);
 
   _addCaps(board, kingIndex, KING, moves, attackable);
 }
 
-inline void MoveGen::_genKnightMoves(const Board &board, U64 knights, U64 attackable) {
+inline void MoveGen::_genKnightMoves(const Board *board, U64 knights, U64 attackable) {
   while (knights) {
     int from = _popLsb(knights);
 
-    U64 moves = board.getAttacksForSquare(KNIGHT, board.getActivePlayer(), from);
+    U64 moves = board->getAttacksForSquare(KNIGHT, board->getActivePlayer(), from);
 
     _addMoves(board, from, KNIGHT, moves, attackable);
   }
 }
 
-inline void MoveGen::_genKnightCaps(const Board &board, U64 knights, U64 attackable) {
+inline void MoveGen::_genKnightCaps(const Board *board, U64 knights, U64 attackable) {
   while (knights) {
     int from = _popLsb(knights);
 
-    U64 moves = board.getAttacksForSquare(KNIGHT, board.getActivePlayer(), from);
+    U64 moves = board->getAttacksForSquare(KNIGHT, board->getActivePlayer(), from);
 
     _addCaps(board, from, KNIGHT, moves, attackable);
   }
 }
 
-inline void MoveGen::_genBishopMoves(const Board &board, U64 bishops, U64 attackable) {
+inline void MoveGen::_genBishopMoves(const Board *board, U64 bishops, U64 attackable) {
   while (bishops) {
     int from = _popLsb(bishops);
 
-    U64 moves = board.getAttacksForSquare(BISHOP, board.getActivePlayer(), from);
+    U64 moves = board->getAttacksForSquare(BISHOP, board->getActivePlayer(), from);
 
     _addMoves(board, from, BISHOP, moves, attackable);
   }
 }
 
-inline void MoveGen::_genBishopCaps(const Board &board, U64 bishops, U64 attackable) {
+inline void MoveGen::_genBishopCaps(const Board *board, U64 bishops, U64 attackable) {
   while (bishops) {
     int from = _popLsb(bishops);
 
-    U64 moves = board.getAttacksForSquare(BISHOP, board.getActivePlayer(), from);
+    U64 moves = board->getAttacksForSquare(BISHOP, board->getActivePlayer(), from);
 
     _addCaps(board, from, BISHOP, moves, attackable);
   }
 }
 
-inline void MoveGen::_genRookMoves(const Board &board, U64 rooks, U64 attackable) {
+inline void MoveGen::_genRookMoves(const Board *board, U64 rooks, U64 attackable) {
   while (rooks) {
     int from = _popLsb(rooks);
 
-    U64 moves = board.getAttacksForSquare(ROOK, board.getActivePlayer(), from);
+    U64 moves = board->getAttacksForSquare(ROOK, board->getActivePlayer(), from);
 
     _addMoves(board, from, ROOK, moves, attackable);
   }
 }
 
-inline void MoveGen::_genRookCaps(const Board &board, U64 rooks, U64 attackable) {
+inline void MoveGen::_genRookCaps(const Board *board, U64 rooks, U64 attackable) {
   while (rooks) {
     int from = _popLsb(rooks);
 
-    U64 moves = board.getAttacksForSquare(ROOK, board.getActivePlayer(), from);
+    U64 moves = board->getAttacksForSquare(ROOK, board->getActivePlayer(), from);
 
     _addCaps(board, from, ROOK, moves, attackable);
   }
 }
 
-inline void MoveGen::_genQueenMoves(const Board &board, U64 queens, U64 attackable) {
+inline void MoveGen::_genQueenMoves(const Board *board, U64 queens, U64 attackable) {
   while (queens) {
     int from = _popLsb(queens);
 
-    U64 moves = board.getAttacksForSquare(QUEEN, board.getActivePlayer(), from);
+    U64 moves = board->getAttacksForSquare(QUEEN, board->getActivePlayer(), from);
 
     _addMoves(board, from, QUEEN, moves, attackable);
   }
 }
 
-inline void MoveGen::_genQueenCaps(const Board &board, U64 queens, U64 attackable) {
+inline void MoveGen::_genQueenCaps(const Board *board, U64 queens, U64 attackable) {
   while (queens) {
     int from = _popLsb(queens);
 
-    U64 moves = board.getAttacksForSquare(QUEEN, board.getActivePlayer(), from);
+    U64 moves = board->getAttacksForSquare(QUEEN, board->getActivePlayer(), from);
 
     _addCaps(board, from, QUEEN, moves, attackable);
   }
 }
 
-inline void MoveGen::_addMoves(const Board &board, int from, PieceType pieceType, U64 moves, U64 attackable) {
+inline void MoveGen::_addMoves(const Board *board, int from, PieceType pieceType, U64 moves, U64 attackable) {
   // Generate non attacks
   U64 nonAttacks = moves & ~attackable;
   while (nonAttacks) {
     int to = _popLsb(nonAttacks);
-    _moves.push_back(Move(from, to, pieceType));
+    _moves->push_back(Move(from, to, pieceType));
   }
 
   // Generate attacks
@@ -349,13 +342,13 @@ inline void MoveGen::_addMoves(const Board &board, int from, PieceType pieceType
     int to = _popLsb(attacks);
 
     Move move(from, to, pieceType, Move::CAPTURE);
-    move.setCapturedPieceType(board.getPieceAtSquare(board.getInactivePlayer(), to));
+    move.setCapturedPieceType(board->getPieceAtSquare(board->getInactivePlayer(), to));
 
-    _moves.push_back(move);
+    _moves->push_back(move);
   }
 }
 
-inline void MoveGen::_addCaps(const Board &board, int from, PieceType pieceType, U64 moves, U64 attackable) {
+inline void MoveGen::_addCaps(const Board *board, int from, PieceType pieceType, U64 moves, U64 attackable) {
 
   // Generate attacks
   U64 attacks = moves & attackable;
@@ -363,7 +356,7 @@ inline void MoveGen::_addCaps(const Board &board, int from, PieceType pieceType,
     int to = _popLsb(attacks);
 
     Move move(from, to, pieceType, Move::CAPTURE);
-    move.setCapturedPieceType(board.getPieceAtSquare(board.getInactivePlayer(), to));
-    _moves.push_back(move);
+    move.setCapturedPieceType(board->getPieceAtSquare(board->getInactivePlayer(), to));
+    _moves->push_back(move);
   }
 }
