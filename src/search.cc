d8ce1333d7ec68b94f52926709abb1986cd1cc45
justno4b <novosti-wxo@yandex.ru>
2024-02-27 17:58:12 +0300

    use in cmhp

--- src/search.cc
+++ src/search.cc
@@ -642,12 +642,12 @@ int Search::_negaMax(Board &board, pV *up_pV, int depth, int alpha, int beta, bo
       // At shallow depth prune highlyish -negative SEE-moves
       if (depth <= 10
           && isQuiet
-          && !board.SEE_GreaterOrEqual(move, (-68 * lmrDepth + 48))) continue;
+          && !board.SEE_GreaterOrEqual(move, (-68 * depth + 48))) continue;
           //&& board.Calculate_SEE(move) < ) continue;
 
       // 5.3. COUNTER-MOVE HISTORY PRUNING
       // Prune quiet moves with poor CMH on the tips of the tree
-      if (depth <= 3 && isQuiet && cmHistory <= (-4096 * depth + 4096)) continue;
+      if (depth <= 3 && isQuiet && cmHistory <= (-4096 * lmrDepth + 4096)) continue;
     }
 
 
