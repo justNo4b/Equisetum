1cf94ca8e2eae13ebb01f4ce624f275b912f2708
justno4b <novosti-wxo@yandex.ru>
2024-03-04 23:22:27 +0300

    try

--- src/search.cc
+++ src/search.cc
@@ -545,6 +545,10 @@ int Search::_negaMax(Board &board, pV *up_pV, int depth, int alpha, int beta, bo
           && !board.SEE_GreaterOrEqual(move, (-68 * depth + 48))) continue;
           //&& board.Calculate_SEE(move) < ) continue;
 
+      if (depth <= 6
+         && !isQuiet
+         && !board.SEE_GreaterOrEqual(move, (alpha - nodeEval - 150 * depth)))
+
       // 5.3. COUNTER-MOVE HISTORY PRUNING
       // Prune quiet moves with poor CMH on the tips of the tree
       if (depth <= 3 && isQuiet && cmHistory <= (-4096 * depth + 4096)) continue;
