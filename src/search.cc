b92d1e9e4e2bfee178d6433d7344cf1e9cdda16e
justno4b <novosti-wxo@yandex.ru>
2024-08-25 22:48:16 +0300

    fix more

--- src/search.cc
+++ src/search.cc
@@ -873,7 +873,7 @@ int Search::_qSearch(Board &board, int alpha, int beta) {
           if (score >= beta) {
             // Add a new tt entry for this node
             if (!_stop){
-                myHASH->HASH_Store(board.getZKey().getValue(), move.getMoveINT(), standPat, BETA, score, 0, MAX_PLY);
+                myHASH->HASH_Store(board.getZKey().getValue(), move.getMoveINT(), nodeEval, BETA, score, 0, MAX_PLY);
             }
             return beta;
           }
