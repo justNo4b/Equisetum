99fcfcbed487ebd3afa9b174bb2f09c3284b7806
justno4b <novosti-wxo@yandex.ru>
2025-04-21 18:58:32 +0300

    move histroy calc for LMR

--- src/search.cc
+++ src/search.cc
@@ -614,7 +614,8 @@ int Search::_negaMax(Board &board, pV *up_pV, int depth, int alpha, int beta, bo
 
         bool giveCheck = movedBoard.colorIsInCheck(movedBoard.getActivePlayer());
         int  moveHistory  = isQuiet ?
-                        _orderingInfo.getHistory(board.getActivePlayer(), move.getFrom(), move.getTo()) :
+                        _orderingInfo.getHistory(board.getActivePlayer(), move.getFrom(), move.getTo()) +
+                        _orderingInfo.getCountermoveHistory(board.getActivePlayer(), pMoveIndx, move.getPieceType(), move.getTo()):
                         0;
 
 
