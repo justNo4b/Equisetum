c0d6e5ca9b654ff1681571e917e75f4835f6dcaa
justno4b <novosti-wxo@yandex.ru>
2025-04-22 18:02:08 +0300

    full CM

--- src/search.cc
+++ src/search.cc
@@ -554,6 +554,7 @@ int Search::_negaMax(Board &board, pV *up_pV, int depth, int alpha, int beta, bo
                         _orderingInfo.getHistory(board.getActivePlayer(), move.getFrom(), move.getTo()) +
                         _orderingInfo.getCountermoveHistory(board.getActivePlayer(), pMoveIndx, move.getPieceType(), move.getTo()):
                         _orderingInfo.getCaptureHistory(move.getPieceType(),move.getCapturedPieceType(), move.getTo());
+    int cmHistory = isQuiet ?  _orderingInfo.getCountermoveHistory(board.getActivePlayer(), pMoveIndx, move.getPieceType(), move.getTo()) : 0;
 
     // 5. PRE-MOVELOOP PRUNING
 
@@ -572,7 +573,7 @@ int Search::_negaMax(Board &board, pV *up_pV, int depth, int alpha, int beta, bo
           && !board.SEE_GreaterOrEqual(move, (-68 * depth + 48))) continue;
 
 
-      if (depth == 1 && isQuiet && moveHistory <= 0) continue;
+      if (depth == 1 && isQuiet && cmHistory <= 0) continue;
 
     }
 
