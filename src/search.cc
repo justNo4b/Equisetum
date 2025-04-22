6ed47190eed37a30d2ac3739fddf1a7b9885c17b
justno4b <novosti-wxo@yandex.ru>
2025-04-22 13:09:54 +0300

    remove some lmr

--- src/search.cc
+++ src/search.cc
@@ -661,10 +661,6 @@ int Search::_negaMax(Board &board, pV *up_pV, int depth, int alpha, int beta, bo
           // Reduce more in the cut-nodes - used by SF/Komodo/etc
           reduction += cutNode;
 
-          // Reduce less if move on the previous ply was bad
-          // Ie hystorycally bad quiet, see- capture or underpromotion
-          reduction -= pMoveScore < -HALFMAX_HISTORY_SCORE;
-
           // if we are improving, reduce a bit less (from Weiss)
           reduction -= improving;
 
