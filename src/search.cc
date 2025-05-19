ead0bbd61a66477ef9b0a4c257bf5aac0fe15006
justno4b <novosti-wxo@yandex.ru>
2025-05-20 01:29:07 +0300

    if cutnode

--- src/search.cc
+++ src/search.cc
@@ -599,7 +599,7 @@ int Search::_negaMax(Board &board, pV *up_pV, int depth, int alpha, int beta, bo
                 singNode = true;
               }else if(!incheckNode && depth > 5 && ttEntry.score >= beta){
                 tDepth -= 2;
-              }else if (!incheckNode && depth > 5){
+              }else if (!incheckNode && depth > 5 && cutNode){
                 tDepth -= 1;
               }
             }
