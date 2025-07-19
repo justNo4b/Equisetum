67d877e90a2068f996099ef4a3a058375182f968
justno4b <novosti-wxo@yandex.ru>
2025-07-19 23:00:24 +0300

    d +

--- src/transptable.cc
+++ src/transptable.cc
@@ -93,7 +93,7 @@ void  HASH::HASH_Store(U64 posKey, int cMove, CutOffState bound, bool isttpv, in
 
       U64 index = posKey & TableMask;
       uint8_t entryAge = hashTable[index].FlagAge >> 4;
-      if (posKey !=  hashTable[index].posKey || depth * (2 + (entryAge != ttAge)) >=  hashTable[index].depth || bound == EXACT){
+      if (posKey !=  hashTable[index].posKey || (depth + (entryAge != ttAge)) * 2 >=  hashTable[index].depth || bound == EXACT){
         uint8_t ttbound = isttpv ? bound | TTPV : bound;
         ttbound = ttbound | (ttAge << 4);
          hashTable[index] = HASH_Entry(posKey, cMove, (int16_t)score, depth, ttbound);
