d3da73f4efb89a87fc0184c7b787b94652510dc9
justno4b <novosti-wxo@yandex.ru>
2025-07-21 16:20:42 +0100

    add description and Elo

--- src/search.cc
+++ src/search.cc
@@ -780,6 +780,7 @@ int Search::_negaMax(Board &board, pV *up_pV, int depth, int alpha, int beta, bo
         // Beta cutoff
         if (score >= beta) {
           // Add this move as a new killer move and update history if move is quiet
+          // No bonus for first move at low depth. Inspired by Alayan history from Ethereal
           int bonus = (depth > 2 || legalCount != 1) ? _getHistoryBonus(depth, nodeEval, alpha) : 0;
           _updateBeta(isQuiet, move, board.getActivePlayer(), pMove, ppMove, ply, bonus);
           // Award counter-move history additionally if we refuted special quite previous move
